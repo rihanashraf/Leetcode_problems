00b72d9ac23bc405c56354e7c88a006dc52b466c
rihanashraf <rihanthachu@gmail.com>
2025-06-12 14:19:54 +0530

    Time: 39 ms (12.11%), Space: 14 MB (31.72%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        # GREG's solution
+        l = r = 0
+        n = len(height)
+        max_left = [0]*n # arrays for storing max_left and max_right at every index
+        max_right = [0]*n   
+
+        for i in range(n):
+            j = -i -1 #when i moves forward, j moves with it backward, from the back
+            max_left[i] = l
+            max_right[j] = r
+            l = max(l, height[i])
+            r = max(r, height[j])
+        
+        res = 0
+        for i in range(n):
+            pot = min(max_left[i], max_right[i])
+            res += max(0, pot - height[i])
+        return res
+
+        #kinda hard, go through again later for greater clarity of concepts
