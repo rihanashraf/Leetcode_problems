28e87309288dfb97dce06a533f3f9ef2209df8ef
rihanashraf <rihanthachu@gmail.com>
2025-06-13 19:07:46 +0530

    Time: 4 ms (28.04%), Space: 12.4 MB (84.16%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        hashmap = {")": "(", "}" : "{", "]": "["}
+        stk = []
+
+        for c in s:
+            if c not in hashmap:
+                stk.append(c)
+            else:
+                if not stk:
+                    return False
+                else:
+                    popped = stk.pop()
+                    if popped != hashmap[c]:
+                        return False
+        return not stk
