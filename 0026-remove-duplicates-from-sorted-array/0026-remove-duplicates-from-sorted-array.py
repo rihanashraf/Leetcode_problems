ec2ea3d9e2bbbe63f32ff44c697316d4c58be37c
rihanashraf <rihanthachu@gmail.com>
2025-06-23 09:47:17 +0530

    Time: 0 ms (100%), Space: 13.9 MB (36.21%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -2,15 +2,12 @@ class Solution(object):
     def removeDuplicates(self, nums):
         """
         :type nums: List[int]
-        :type val: int
         :rtype: int
         """
-        #concept of k pointer in place?
-
         k = 0
-
         for i in range(len(nums)):
-            if nums[i] != nums[k]:
+            if nums[k] != nums[i]:
                 nums[k+1] = nums[i]
-                k+=1
-        return k+1 
+                k +=1
+        return k+1
+        
