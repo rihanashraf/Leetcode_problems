24eb3e293620b351f6ff42bb21480e0a092baf53
rihanashraf <rihanthachu@gmail.com>
2025-07-04 14:17:39 +0530

    Time: 12 ms (5.4%), Space: 12.7 MB (20.36%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,17 +5,16 @@ class Solution(object):
         :rtype: bool
         """
 
-        hashmap = {")": "(", "}" : "{", "]": "["}
-        stk = []
+        dicti = {"]":"[", "}":"{", ")" : "("}
+        arr = []
 
-        for c in s:
-            if c not in hashmap:
-                stk.append(c)
+        for char in s:
+            if char not in dicti:
+                arr.append(char)
             else:
-                if not stk:
-                    return False
+                if dicti[char] in arr and dicti[char]==arr[-1]:
+                    print("yes")
+                    arr.pop()
                 else:
-                    popped = stk.pop()
-                    if popped != hashmap[c]:
-                        return False
-        return not stk
+                    return False
+        return not arr
