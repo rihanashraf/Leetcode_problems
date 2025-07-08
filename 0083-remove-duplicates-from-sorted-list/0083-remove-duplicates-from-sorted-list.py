86bc5ae3492329b4db86ac40e94de0beef79f89f
rihanashraf <rihanthachu@gmail.com>
2025-07-08 11:44:48 +0530

    Time: 1 ms (55.57%), Space: 12.4 MB (58.6%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -10,9 +10,10 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         curr = head
-        while curr and curr.next:  #Check if they are valid nodes, if they are not None
+        while curr and curr.next:
             if curr.val == curr.next.val:
                 curr.next = curr.next.next
             else:
                 curr = curr.next
-        return head 
+                
+        return head
