9777a75ab2714272115117afc2f5944a531da555
rihanashraf <rihanthachu@gmail.com>
2025-07-08 11:11:10 +0530

    Time: 26 ms (73.15%), Space: 12.7 MB (77.52%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -8,20 +8,15 @@ class Solution(object):
         dicti = {}
         for letter in magazine:
             if letter not in dicti:
-                dicti[letter] = 1
+                dicti[letter]  = 1
             else:
                 dicti[letter] +=1
-        
+
         for letter in ransomNote:
-            if letter in dicti and dicti[letter] !=0:
-                dicti[letter] -= 1
+            if letter in dicti and dicti[letter] >0:
+                dicti[letter] -=1
             else:
                 return False
-        return True
-        
-
-
-        
-            
+        return True           
             
         
