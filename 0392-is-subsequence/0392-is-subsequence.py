ca0e955ba6a710a258bf9a0e13f885ec837f7b65
rihanashraf <rihanthachu@gmail.com>
2025-07-26 22:39:14 +0530

    Time: 4 ms (18.66%), Space: 12.6 MB (45.73%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,19 +5,16 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-
         i = 0
         j = 0
         while i < len(s) and j < len(t):
-            letter = s[i]
-            andi = t[j]
-            if letter == andi:
-                i +=1
-                j+=1
-            else:
-                j +=1
-        
-        return True if i == len(s) else False
+                if t[j] == s[i]:
+                    i+=1
+                    j+=1
+                else:
+                    j+=1
 
-
-        
+        if i == len(s):
+            return True
+        else:
+            return False
