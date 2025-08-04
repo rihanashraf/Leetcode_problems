cd6614ae10c2b7f5f19145f0a5bd1e1bebd141a5
rihanashraf <rihanthachu@gmail.com>
2025-08-04 15:45:23 +0530

    Time: 34 ms (85.7%), Space: 19.1 MB (45.43%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -5,25 +5,17 @@ class Solution(object):
         :rtype: int
         """
         max_profit = 0
-        min_number = float('inf')
-
+        min_price = float("inf")
         for price in prices:
-            if price < min_number:
-                min_number = price
+            if price < min_price:
+                min_price = price
 
-            profit = price - min_number
+            profit = price - min_price
 
             if profit > max_profit:
                 max_profit = profit
 
         return max_profit
 
-
-        
-
-        
-
-
-            
 
         
