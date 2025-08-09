ac6aeecb23fdd4d3469916b90b63baff7ab4cdac
rihanashraf <rihanthachu@gmail.com>
2025-08-09 20:28:32 +0530

    Time: 4 ms (81.66%), Space: 16.1 MB (9.17%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        D = defaultdict(list)
+        for a,b in prerequisites:
+            D[a].append(b)
+        
+
+        unvisited = 0
+        visiting = 1
+        visited = 2
+
+        states = [unvisited] * numCourses
+
+
+        def dfs(node):
+            state = states[node]
+            if state == visited:
+                return True
+            if state == visiting:
+                return False
+            states[node] = visiting
+
+            for nei in D[node]:
+                if not dfs(nei):
+                    return False
+            
+            states[node] = visited
+            order.append(node)
+            return True
+
+        order = []
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+
+        return order
