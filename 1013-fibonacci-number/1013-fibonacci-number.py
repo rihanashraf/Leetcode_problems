a7bed4840144d8f52b5212cff8bd3a7b06648395
rihanashraf <rihanthachu@gmail.com>
2025-08-31 16:16:21 +0530

    Time: 12 ms (85.03%), Space: 12.6 MB (9.46%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -6,14 +6,18 @@ class Solution(object):
         """
         #bottom up
 
-        arr = [0] * (n+1)
-        arr[0] = 0
-        if n !=0:
-            arr[1] = 1
+        if n == 0:
+            return 0
+        if n==1:
+            return 1
+        prev = 0
+        curr = 1
         for i in range(2, n+1):
-            arr[i] = arr[i-1] + arr[i-2]
+            temp = curr
+            curr = prev + curr
+            prev = temp
 
-        return arr[n]
+        return curr
 
             
 
